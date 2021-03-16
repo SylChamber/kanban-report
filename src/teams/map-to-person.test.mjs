1a98b212a3b507b70dddbc9618ba2c023e42182a
Sylvain Chamberland <gentx2@gmail.com>
2021-03-16 13:48:37 -0400

    test reordering in map-to-person

--- src/teams/map-to-person.test.mjs
+++ src/teams/map-to-person.test.mjs
@@ -1,40 +1,42 @@
 import mapToPerson from './map-to-person.mjs'
 import { assert } from 'chai'
 
-suite('mapToPerson', function () {
-  test('copies name', function () {
-    const identity = createIdentity()
-    const person = mapToPerson(identity)
-    assert.strictEqual(person.name, identity.displayName)
-  })
+suite('teams', function () {
+  suite('mapToPerson', function () {
+    test('copies email', function () {
+      const identity = createIdentity()
+      const person = mapToPerson(identity)
+      assert.strictEqual(person.email, identity.uniqueName)
+    })
 
-  test('ignores name if absent', function () {
-    const identity = { uniqueName: 'john.doe@example.com' }
-    const person = mapToPerson(identity)
-    assert.isUndefined(person.name)
-  })
+    test('copies name', function () {
+      const identity = createIdentity()
+      const person = mapToPerson(identity)
+      assert.strictEqual(person.name, identity.displayName)
+    })
 
-  test('copies email', function () {
-    const identity = createIdentity()
-    const person = mapToPerson(identity)
-    assert.strictEqual(person.email, identity.uniqueName)
-  })
+    test('ignores email if absent', function () {
+      const identity = { displayName: 'John Doe' }
+      const person = mapToPerson(identity)
+      assert.isUndefined(person.email)
+    })
 
-  test('ignores email if absent', function () {
-    const identity = { displayName: 'John Doe' }
-    const person = mapToPerson(identity)
-    assert.isUndefined(person.email)
-  })
+    test('ignores name if absent', function () {
+      const identity = { uniqueName: 'john.doe@example.com' }
+      const person = mapToPerson(identity)
+      assert.isUndefined(person.name)
+    })
 
-  test('returns undefined if identity undefined', function () {
-    const person = mapToPerson(undefined)
-    assert.isUndefined(person)
-  })
+    test('returns undefined if identity undefined', function () {
+      const person = mapToPerson(undefined)
+      assert.isUndefined(person)
+    })
 
-  function createIdentity () {
-    return {
-      displayName: 'John Doe',
-      uniqueName: 'john.doe@example.com'
+    function createIdentity () {
+      return {
+        displayName: 'John Doe',
+        uniqueName: 'john.doe@example.com'
+      }
     }
-  }
+  })
 })
