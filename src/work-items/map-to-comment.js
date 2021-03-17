d17da1c91cb70d760b3fd64dbb75135f67a9f952
Sylvain Chamberland <gentx2@gmail.com>
2021-03-17 10:22:51 -0400

    implemented rest of mapToComment

--- src/work-items/map-to-comment.js
+++ src/work-items/map-to-comment.js
@@ -1,18 +1,50 @@
+import mapToPerson from '../teams/map-to-person.js'
+
+/**
+ * @typedef {import("../teams/map-to-person").Person} Person
+ */
+
 /**
  * Represents a work item comment.
  * @typedef Comment
  * @property {number} id - ID of the comment in Azure DevOps.
  * @property {number} workItemId - ID of the work item to which the comment was posted.
+ * @property {Person} createdBy - The person who created the comment.
+ * @property {Date} createdDate - The creation date of the comment.
+ * @property {Person} [modifiedBy] - The personn who modified the comment, if applicable.
+ * @property {Date} [modifiedDate] - The date when the comment was modified, if applicable.
+ * @property {string} text - Text of the comment.
+ * @property {string} url - The URL of the comment in Azure DevOps.
+ * @property {number} version - The version number of the comment.
  */
 
 /**
  * Maps a work item comment from Azure DevOps to an object that represents a comment.
- * @param comment - Work item comment in Azure DevOps.
+ * @param workItemComment - Work item comment in Azure DevOps.
  * @returns {Comment} An object that represents the work item comment.
  */
-export default function mapToComment (comment) {
-  return {
-    id: comment.id,
-    workItemId: comment.workItemId
+export default function mapToComment (workItemComment) {
+  const comment = {
+    id: workItemComment.id,
+    workItemId: workItemComment.workItemId,
+    createdDate: new Date(workItemComment.createdDate),
+    createdBy: mapToPerson(workItemComment.createdBy),
+    text: workItemComment.text,
+    url: workItemComment.url,
+    version: workItemComment.version
   }
+
+  if (workItemComment.modifiedDate) {
+    Object.assign(comment, {
+      modifiedDate: new Date(workItemComment.modifiedDate)
+    })
+  }
+
+  if (workItemComment.modifiedBy) {
+    Object.assign(comment, {
+      modifiedBy: mapToPerson(workItemComment.modifiedBy)
+    })
+  }
+
+  return comment
 }
--- src/work-items/map-to-comment.test.js
+++ src/work-items/map-to-comment.test.js
@@ -7,20 +7,113 @@ suite('work items', function () {
 
     test('copies id', function () {
       const workItemComment = createBasicWorkItemComment()
-      const comment = mapToComment(workItemComment)
-      assert.strictEqual(comment.id, workItemComment.id)
+      const realComment = mapToComment(workItemComment)
+      assert.strictEqual(realComment.id, workItemComment.id)
     })
 
     test('copies work item id', function () {
       const workItemComment = createBasicWorkItemComment()
-      const comment = mapToComment(workItemComment)
-      assert.strictEqual(comment.workItemId, workItemComment.workItemId)
+      const realComment = mapToComment(workItemComment)
+      assert.strictEqual(realComment.workItemId, workItemComment.workItemId)
+    })
+
+    test('copies text', function () {
+      const workItemComment = createBasicWorkItemComment()
+      const realComment = mapToComment(workItemComment)
+      assert.strictEqual(realComment.text, workItemComment.text)
+    })
+
+    test('copies created date', function () {
+      const createdDate = '2021-02-21T21:21:21Z'
+      const expectedDate = new Date(createdDate)
+      const workItemComment = Object.assign(
+        createBasicWorkItemComment(),
+        {
+          createdDate: createdDate
+        })
+      const realComment = mapToComment(workItemComment)
+      assert.strictEqual(realComment.createdDate.valueOf(), expectedDate.valueOf())
+    })
+
+    test('copies created by', function () {
+      const expectedCreatedBy = {
+        name: 'John Doe',
+        email: 'john.doe@example.com'
+      }
+      const workItemComment = Object.assign(
+        createBasicWorkItemComment(),
+        {
+          createdBy: {
+            displayName: expectedCreatedBy.name,
+            uniqueName: expectedCreatedBy.email
+          }
+        })
+      const realComment = mapToComment(workItemComment)
+      assert.deepEqual(realComment.createdBy, expectedCreatedBy)
+    })
+
+    test('copies modified date', function () {
+      const modifiedDate = '2021-02-22T22:22:22Z'
+      const expectedDate = new Date(modifiedDate)
+      const workItemComment = Object.assign(
+        createBasicWorkItemComment(),
+        {
+          modifiedDate: modifiedDate
+        })
+      const realComment = mapToComment(workItemComment)
+      assert.strictEqual(realComment.modifiedDate.valueOf(), expectedDate.valueOf())
+    })
+
+    test('ignores modified date if absent', function () {
+      const workItemComment = createBasicWorkItemComment()
+      const realComment = mapToComment(workItemComment)
+      assert.isFalse(Object.keys(realComment).includes('modifiedDate'))
+    })
+
+    test('copies modified by', function () {
+      const expectedModifiedBy = {
+        name: 'John Doe',
+        email: 'john.doe@example.com'
+      }
+      const workItemComment = Object.assign(
+        createBasicWorkItemComment(),
+        {
+          modifiedBy: {
+            displayName: expectedModifiedBy.name,
+            uniqueName: expectedModifiedBy.email
+          }
+        }
+      )
+      const realComment = mapToComment(workItemComment)
+      assert.deepEqual(realComment.modifiedBy, expectedModifiedBy)
+    })
+
+    test('ignores modified by if absent', function () {
+      const workItemComment = createBasicWorkItemComment()
+      const realComment = mapToComment(workItemComment)
+      assert.isUndefined(realComment.modifiedBy)
+      assert.isFalse(Object.keys(realComment).includes('modifiedBy'))
+    })
+
+    test('copies url', function () {
+      const workItemComment = createBasicWorkItemComment()
+      const realComment = mapToComment(workItemComment)
+      assert.strictEqual(realComment.url, workItemComment.url)
+    })
+
+    test('copies version number', function () {
+      const workItemComment = createBasicWorkItemComment()
+      const realComment = mapToComment(workItemComment)
+      assert.strictEqual(realComment.version, workItemComment.version)
     })
 
     function createBasicWorkItemComment () {
       return {
         id: 5,
-        workItemId: 512
+        workItemId: 512,
+        version: 2,
+        text: '<div>some text</div>',
+        url: 'https://dev.azure.com/org/project/_apis/wit/workItems/512/comments/5'
       }
     }
   })
