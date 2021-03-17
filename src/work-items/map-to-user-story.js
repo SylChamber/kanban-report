296f36f06896865bf5585ec478a19c89af74b23b
Sylvain Chamberland <gentx2@gmail.com>
2021-03-17 15:27:52 -0400

    finished first version of mapToUserStory

--- src/work-items/map-to-user-story.js
+++ src/work-items/map-to-user-story.js
@@ -1,17 +1,38 @@
+import mapToBoard from './map-to-board.js'
+import mapToPerson from '../teams/map-to-person.js'
+
+/**
+ * @typedef {import('../teams/map-to-person').Person} Person
+ */
+
+/**
+ * @typedef {import('./map-to-board').BoardLocation} BoardLocation
+ */
+
 /**
  * Represents a user story in Azure DevOps.
  * @typedef {Object} UserStory
  * @property {number} id - The ID of the user story in Azure DevOps.
  * @property {string} [acceptanceCriteria] - The acceptance criteria of the user story.
+ * @property {Person} [activatedBy] - The person who activated the user story, e.g. who began work.
  * @property {Date} [activatedDate] - The date the user story was activated, e.g. when work began.
  * @property {string} areaPath - The area path of the user story, that categorizes the user story.
+ * @property {Person} [assignedTo] - The person the user story is assigned to.
+ * @property {BoardLocation} board - The location of the user story on the board.
  * @property {Date} [changedDate] - The date the user story was changed, if applicable.
+ * @property {Person} [closedBy] - The person who closed the user story, e.g. marked it as finished.
+ * @property {Date} [closedDate] - The date the user story was closed (e.g. finished), if applicable.
+ * @property {Person} createdBy - The person that created the user story.
  * @property {Date} createdDate - The date the user story was created.
  * @property {string} [description] - The description of the user story.
- * @property {string} title - The title of the user story.
+ * @property {Date} [firstActivatedDate] - The date the user story was first activated (e.g. work first began).
+ * @property {string} project - The Azure DevOps project the user story is in.
+ * @property {number} revision - The revision number of the user story in Azure DevOps.
  * @property {string} state - The state of the user story, for example 'New', 'Active', 'Closed'.
- * @property {Date} [stateChangedDate] - The date the state of the user story was changed.
+ * @property {Date} [stateChangeDate] - The date the state of the user story was changed.
  * @property {string} [stateReason] - The reason for the state change of the story, for example 'Implementation started'.
+ * @property {string[]} [tags] - Tags that are associated with the user story.
+ * @property {string} title - The title of the user story.
  * @property {string} workItemType - The type of the work item; should be 'User Story'.
  */
 
@@ -21,32 +42,101 @@
  * @returns {UserStory} An object that represents the user story in Azure DevOps.
  */
 export default function mapToUserStory (item) {
-  const activatedDateKey = 'Microsoft.VSTS.Common.ActivatedDate'
-  const changedDateKey = 'System.ChangedDate'
   const story = {
     id: item.id,
-    acceptanceCriteria: item.fields['Microsoft.VSTS.Common.AcceptanceCriteria'],
-    areaPath: item.fields['System.AreaPath'],
-    createdDate: new Date(item.fields['System.CreatedDate']),
-    description: item.fields['System.Description'],
-    state: item.fields['System.State'],
-    stateChangedDate: new Date(item.fields['Microsoft.VSTS.Common.StateChangeDate']),
-    stateReason: item.fields['System.Reason'],
-    title: item.fields['System.Title'],
-    workItemType: item.fields['System.WorkItemType']
+    acceptanceCriteria: item.fields[fieldKeys.acceptanceCriteria],
+    areaPath: item.fields[fieldKeys.areaPath],
+    board: mapToBoard(item),
+    createdBy: mapToPerson(item.fields[fieldKeys.createdBy]),
+    createdDate: new Date(item.fields[fieldKeys.createdDate]),
+    description: item.fields[fieldKeys.description],
+    project: item.fields[fieldKeys.teamProject],
+    revision: item.rev,
+    state: item.fields[fieldKeys.state],
+    stateChangeDate: new Date(item.fields[fieldKeys.stateChangeDate]),
+    stateReason: item.fields[fieldKeys.stateReason],
+    title: item.fields[fieldKeys.title],
+    workItemType: item.fields[fieldKeys.workItemType]
   }
 
-  if (item.fields[activatedDateKey]) {
+  if (item.fields[fieldKeys.activatedBy]) {
     Object.assign(story, {
-      activatedDate: new Date(item.fields[activatedDateKey])
+      activatedBy: mapToPerson(item.fields[fieldKeys.activatedBy])
     })
   }
 
-  if (item.fields[changedDateKey]) {
+  if (item.fields[fieldKeys.activatedDate]) {
     Object.assign(story, {
-      changedDate: new Date(item.fields[changedDateKey])
+      activatedDate: new Date(item.fields[fieldKeys.activatedDate])
+    })
+  }
+
+  if (item.fields[fieldKeys.assignedTo]) {
+    Object.assign(story, {
+      assignedTo: mapToPerson(item.fields[fieldKeys.assignedTo])
+    })
+  }
+
+  if (item.fields[fieldKeys.changedDate]) {
+    Object.assign(story, {
+      changedDate: new Date(item.fields[fieldKeys.changedDate])
+    })
+  }
+
+  if (item.fields[fieldKeys.closedBy]) {
+    Object.assign(story, {
+      closedBy: mapToPerson(item.fields[fieldKeys.closedBy])
+    })
+  }
+
+  if (item.fields[fieldKeys.closedDate]) {
+    Object.assign(story, {
+      closedDate: new Date(item.fields[fieldKeys.closedDate])
+    })
+  }
+
+  if (item.fields[fieldKeys.firstActivatedDate]) {
+    Object.assign(story, {
+      firstActivatedDate: new Date(item.fields[fieldKeys.firstActivatedDate])
+    })
+  }
+
+  if (item.fields[fieldKeys.tags]) {
+    Object.assign(story, {
+      tags: item.fields[fieldKeys.tags].split(';').map(trim)
     })
   }
 
   return story
+}
+
+const trim = s => s.trim()
+
+const fieldKeys = {
+  acceptanceCriteria: 'Microsoft.VSTS.Common.AcceptanceCriteria',
+  activatedBy: 'Microsoft.VSTS.Common.ActivatedBy',
+  activatedDate: 'Microsoft.VSTS.Common.ActivatedDate',
+  areaPath: 'System.AreaPath',
+  assignedTo: 'System.AssignedTo',
+  boardColumn: 'System.BoardColumn',
+  boardColumnDone: 'System.BoardColumnDone',
+  boardLane: 'System.BoardLane',
+  changedBy: 'System.ChangedBy',
+  changedDate: 'System.ChangedDate',
+  closedBy: 'Microsoft.VSTS.Common.ClosedBy',
+  closedDate: 'Microsoft.VSTS.Common.ClosedDate',
+  createdBy: 'System.CreatedBy',
+  createdDate: 'System.CreatedDate',
+  description: 'System.Description',
+  firstActivatedDate: 'Custom.FirstActivatedDate',
+  resolvedBy: 'Microsoft.VSTS.Common.ResolvedBy',
+  resolvedDate: 'Microsoft.VSTS.Common.ResolvedDate',
+  stackRank: 'Microsoft.VSTS.Common.StackRank',
+  state: 'System.State',
+  stateChangeDate: 'Microsoft.VSTS.Common.StateChangeDate',
+  stateReason: 'System.Reason',
+  tags: 'System.Tags',
+  teamProject: 'System.TeamProject',
+  title: 'System.Title',
+  workItemType: 'System.WorkItemType'
 }
--- src/work-items/map-to-user-story.test.js
+++ src/work-items/map-to-user-story.test.js
@@ -1,11 +1,40 @@
 import { assert } from 'chai'
 import mapToUserStory from './map-to-user-story.js'
 
+/**
+ * @typedef {import('../teams/map-to-person').Person} Person
+ */
+
 suite('work items', function () {
   suite('mapToUserStory', function () {
-    test('function exists', function () {
-      assert.isFunction(mapToUserStory)
-    })
+    const fieldKeys = {
+      acceptanceCriteria: 'Microsoft.VSTS.Common.AcceptanceCriteria',
+      activatedBy: 'Microsoft.VSTS.Common.ActivatedBy',
+      activatedDate: 'Microsoft.VSTS.Common.ActivatedDate',
+      areaPath: 'System.AreaPath',
+      assignedTo: 'System.AssignedTo',
+      boardColumn: 'System.BoardColumn',
+      boardColumnDone: 'System.BoardColumnDone',
+      boardLane: 'System.BoardLane',
+      changedBy: 'System.ChangedBy',
+      changedDate: 'System.ChangedDate',
+      closedBy: 'Microsoft.VSTS.Common.ClosedBy',
+      closedDate: 'Microsoft.VSTS.Common.ClosedDate',
+      createdBy: 'System.CreatedBy',
+      createdDate: 'System.CreatedDate',
+      description: 'System.Description',
+      firstActivatedDate: 'Custom.FirstActivatedDate',
+      resolvedBy: 'Microsoft.VSTS.Common.ResolvedBy',
+      resolvedDate: 'Microsoft.VSTS.Common.ResolvedDate',
+      stackRank: 'Microsoft.VSTS.Common.StackRank',
+      state: 'System.State',
+      stateChangeDate: 'Microsoft.VSTS.Common.StateChangeDate',
+      stateReason: 'System.Reason',
+      tags: 'System.Tags',
+      teamProject: 'System.TeamProject',
+      title: 'System.Title',
+      workItemType: 'System.WorkItemType'
+    }
 
     test('copies id', function () {
       const workItem = createBasicWorkItem()
@@ -16,68 +45,101 @@ suite('work items', function () {
     test('copies work item type', function () {
       const workItem = createBasicWorkItem()
       const realStory = mapToUserStory(workItem)
-      assert.strictEqual(realStory.workItemType, workItem.fields['System.WorkItemType'])
+      assert.strictEqual(realStory.workItemType, workItem.fields[fieldKeys.workItemType])
     })
 
     test('copies title', function () {
       const workItem = createBasicWorkItem()
       const realStory = mapToUserStory(workItem)
-      assert.strictEqual(realStory.title, workItem.fields['System.Title'])
+      assert.strictEqual(realStory.title, workItem.fields[fieldKeys.title])
     })
 
     test('copies area path', function () {
       const workItem = createBasicWorkItem()
       const realStory = mapToUserStory(workItem)
-      assert.strictEqual(realStory.areaPath, workItem.fields['System.AreaPath'])
+      assert.strictEqual(realStory.areaPath, workItem.fields[fieldKeys.areaPath])
     })
 
     test('copies state', function () {
       const workItem = createBasicWorkItem()
       Object.assign(workItem.fields, {
-        'System.State': 'Active'
+        [fieldKeys.state]: 'Active'
       })
       const realStory = mapToUserStory(workItem)
-      assert.strictEqual(realStory.state, workItem.fields['System.State'])
+      assert.strictEqual(realStory.state, workItem.fields[fieldKeys.state])
     })
 
     test('copies state reason', function () {
       const workItem = createBasicWorkItem()
       Object.assign(workItem.fields, {
-        'System.Reason': 'Implementation started'
+        [fieldKeys.stateReason]: 'Implementation started'
       })
       const realStory = mapToUserStory(workItem)
-      assert.strictEqual(realStory.stateReason, workItem.fields['System.Reason'])
+      assert.strictEqual(realStory.stateReason, workItem.fields[fieldKeys.stateReason])
     })
 
-    test('copies state changed date', function () {
-      const stateDateChangedKey = 'Microsoft.VSTS.Common.StateChangeDate'
-      const stateDateChange = '2020-02-20T20:20:20Z'
-      const expectedDate = new Date(stateDateChange)
+    test('copies state change date', function () {
+      const stateChangeDate = '2020-02-20T20:20:20Z'
+      const expectedDate = new Date(stateChangeDate)
       const workItem = createBasicWorkItem()
       Object.assign(workItem.fields, {
-        [stateDateChangedKey]: stateDateChange
+        [fieldKeys.stateChangeDate]: stateChangeDate
       })
       const realStory = mapToUserStory(workItem)
-      assert.strictEqual(realStory.stateChangedDate.valueOf(), expectedDate.valueOf())
+      assert.strictEqual(realStory.stateChangeDate.valueOf(), expectedDate.valueOf())
+    })
+
+    test('copies closed date', function () {
+      const closedDate = '2020-12-12T12:12:12Z'
+      const expectedDate = new Date(closedDate)
+      const workItem = createBasicWorkItem()
+      Object.assign(workItem.fields, {
+        [fieldKeys.closedDate]: closedDate
+      })
+      const realStory = mapToUserStory(workItem)
+      assert.strictEqual(realStory.closedDate.valueOf(), expectedDate.valueOf())
+    })
+
+    test('ignores closed date if undefined', function () {
+      const workItem = createBasicWorkItem()
+      const realStory = mapToUserStory(workItem)
+      assert.isUndefined(realStory.closedDate)
+      assert.isFalse(Object.keys(realStory).includes('closedDate'))
+    })
+
+    test('copies created by', function () {
+      const createdBy = {
+        displayName: 'John Doe',
+        uniqueName: 'john.doe@example.com'
+      }
+      const expectedCreatedBy = {
+        name: createdBy.displayName,
+        email: createdBy.uniqueName
+      }
+      const workItem = createBasicWorkItem()
+      Object.assign(workItem.fields, {
+        [fieldKeys.createdBy]: createdBy
+      })
+      const realStory = mapToUserStory(workItem)
+      assert.deepEqual(realStory.createdBy, expectedCreatedBy)
     })
 
     test('copies description', function () {
       const workItem = createBasicWorkItem()
       Object.assign(workItem.fields, {
-        'System.Description': 'some description'
+        [fieldKeys.description]: 'some description'
       })
       const realStory = mapToUserStory(workItem)
-      assert.strictEqual(realStory.description, workItem.fields['System.Description'])
+      assert.strictEqual(realStory.description, workItem.fields[fieldKeys.description])
     })
 
     test('copies acceptance criteria', function () {
-      const acceptanceCriteriaKey = 'Microsoft.VSTS.Common.AcceptanceCriteria'
       const workItem = createBasicWorkItem()
       Object.assign(workItem.fields, {
-        [acceptanceCriteriaKey]: 'some acceptance criteria'
+        [fieldKeys.acceptanceCriteria]: 'some acceptance criteria'
       })
       const realStory = mapToUserStory(workItem)
-      assert.strictEqual(realStory.acceptanceCriteria, workItem.fields[acceptanceCriteriaKey])
+      assert.strictEqual(realStory.acceptanceCriteria, workItem.fields[fieldKeys.acceptanceCriteria])
     })
 
     test('copies created date', function () {
@@ -85,19 +147,18 @@ suite('work items', function () {
       const expectedDate = new Date(createdDate)
       const workItem = createBasicWorkItem()
       Object.assign(workItem.fields, {
-        'System.CreatedDate': createdDate
+        [fieldKeys.createdDate]: createdDate
       })
       const realStory = mapToUserStory(workItem)
       assert.strictEqual(realStory.createdDate.valueOf(), expectedDate.valueOf())
     })
 
     test('copies activated date', function () {
-      const activatedDateKey = 'Microsoft.VSTS.Common.ActivatedDate'
       const activatedDate = '2020-08-08T08:08:08Z'
       const expectedDate = new Date(activatedDate)
       const workItem = createBasicWorkItem()
       Object.assign(workItem.fields, {
-        [activatedDateKey]: activatedDate
+        [fieldKeys.activatedDate]: activatedDate
       })
       const realStory = mapToUserStory(workItem)
       assert.strictEqual(realStory.activatedDate.valueOf(), expectedDate.valueOf())
@@ -109,12 +170,80 @@ suite('work items', function () {
       assert.isUndefined(realStory.activatedDate)
     })
 
+    test('copies activated by', function () {
+      const expectedActivatedBy = {
+        name: 'Jane Dove',
+        email: 'jane.dove@island.org'
+      }
+      const workItem = createBasicWorkItem()
+      Object.assign(workItem.fields, {
+        [fieldKeys.activatedBy]: {
+          displayName: expectedActivatedBy.name,
+          uniqueName: expectedActivatedBy.email
+        }
+      })
+      const realStory = mapToUserStory(workItem)
+      assert.deepEqual(realStory.activatedBy, expectedActivatedBy)
+    })
+
+    test('ignores activated by if undefined', function () {
+      const workItem = createBasicWorkItem()
+      const realStory = mapToUserStory(workItem)
+      assert.isUndefined(realStory.activatedBy)
+      assert.isFalse(Object.keys(realStory).includes('activatedBy'))
+    })
+
+    test('copies assigned to', function () {
+      const expectedAssignedTo = {
+        name: 'Q',
+        email: 'q@continuum.universe'
+      }
+      const workItem = createBasicWorkItem()
+      Object.assign(workItem.fields, {
+        [fieldKeys.assignedTo]: {
+          displayName: expectedAssignedTo.name,
+          uniqueName: expectedAssignedTo.email
+        }
+      })
+      const realStory = mapToUserStory(workItem)
+      assert.deepEqual(realStory.assignedTo, expectedAssignedTo)
+    })
+
+    test('ignores assigned to if undefined', function () {
+      const workItem = createBasicWorkItem()
+      const realStory = mapToUserStory(workItem)
+      assert.isUndefined(realStory.assignedTo)
+      assert.isFalse(Object.keys(realStory).includes('assignedTo'))
+    })
+
+    // eslint-disable-next-line mocha/no-setup-in-describe
+    createBoardSamples().forEach(function (board) {
+      test(`copies board with ${board.name}`, function () {
+        const workItem = createBasicWorkItem()
+        Object.assign(workItem.fields, board.data)
+        const realStory = mapToUserStory(workItem)
+        assert.deepEqual(realStory.board, board.expected)
+      })
+    })
+
+    test('copies board column', function () {
+      const expectedBoard = {
+        column: 'Doing'
+      }
+      const workItem = createBasicWorkItem()
+      Object.assign(workItem.fields, {
+        [fieldKeys.boardColumn]: expectedBoard.column
+      })
+      const realStory = mapToUserStory(workItem)
+      assert.deepEqual(realStory.board, expectedBoard)
+    })
+
     test('copies changed date', function () {
       const changedDate = '2021-03-31T12:12:12Z'
       const expectedDate = new Date(changedDate)
       const workItem = createBasicWorkItem()
       Object.assign(workItem.fields, {
-        'System.ChangedDate': changedDate
+        [fieldKeys.changedDate]: changedDate
       })
       const realStory = mapToUserStory(workItem)
       assert.strictEqual(realStory.changedDate.valueOf(), expectedDate.valueOf())
@@ -127,6 +256,82 @@ suite('work items', function () {
       assert.isFalse(Object.keys(realStory).includes('changedDate'))
     })
 
+    test('copies closed by', function () {
+      const expectedClosedBy = {
+        name: 'Jean-Luc Picard',
+        email: 'jl.picard@enterprise.starfleet'
+      }
+      const workItem = createBasicWorkItem()
+      Object.assign(workItem.fields, {
+        [fieldKeys.closedBy]: {
+          displayName: expectedClosedBy.name,
+          uniqueName: expectedClosedBy.email
+        }
+      })
+      const realStory = mapToUserStory(workItem)
+      assert.deepEqual(realStory.closedBy, expectedClosedBy)
+    })
+
+    test('ignores closed by if undefined', function () {
+      const workItem = createBasicWorkItem()
+      const realStory = mapToUserStory(workItem)
+      assert.isUndefined(realStory.closedBy)
+      assert.isFalse(Object.keys(realStory).includes('closedBy'))
+    })
+
+    test('copies first activated date', function () {
+      const firstActivatedDate = '2018-03-18T18:18:18Z'
+      const expectedDate = new Date(firstActivatedDate)
+      const workItem = createBasicWorkItem()
+      Object.assign(workItem.fields, {
+        [fieldKeys.firstActivatedDate]: firstActivatedDate
+      })
+      const realStory = mapToUserStory(workItem)
+      assert.strictEqual(realStory.firstActivatedDate.valueOf(), expectedDate.valueOf())
+    })
+
+    test('ignores first activated date if undefined', function () {
+      const workItem = createBasicWorkItem()
+      const realStory = mapToUserStory(workItem)
+      assert.isUndefined(realStory.firstActivatedDate)
+      assert.isFalse(Object.keys(realStory).includes('firstActivatedDate'))
+    })
+
+    test('copies project', function () {
+      const workItem = createBasicWorkItem()
+      Object.assign(workItem.fields, {
+        [fieldKeys.teamProject]: 'Project'
+      })
+      const realStory = mapToUserStory(workItem)
+      assert.strictEqual(realStory.project, workItem.fields[fieldKeys.teamProject])
+    })
+
+    test('copies revision', function () {
+      const workItem = createBasicWorkItem()
+      Object.assign(workItem, {
+        rev: 42
+      })
+      const realStory = mapToUserStory(workItem)
+      assert.strictEqual(realStory.revision, workItem.rev)
+    })
+
+    test('copies tags', function () {
+      const expectedTags = ['todo', 'urgent', 'did i say it\'s urgent?']
+      const workItem = createBasicWorkItem()
+      Object.assign(workItem.fields, {
+        [fieldKeys.tags]: expectedTags.join(' ;')
+      })
+      const realStory = mapToUserStory(workItem)
+      assert.deepEqual(realStory.tags, expectedTags)
+    })
+
+    test('ignore tags if undefined', function () {
+      const workItem = createBasicWorkItem()
+      const realStory = mapToUserStory(workItem)
+      assert.isUndefined(realStory.tags)
+      assert.isFalse(Object.keys(realStory).includes('tags'))
+    })
+
     function createBasicWorkItem () {
       return {
         id: 512,
@@ -137,6 +342,44 @@ suite('work items', function () {
           'System.State': 'New'
         }
       }
+    }
+
+    function createBoardSamples () {
+      return [
+        {
+          name: 'column',
+          data: {
+            [fieldKeys.boardColumn]: 'New'
+          },
+          expected: {
+            column: 'New'
+          }
+        },
+        {
+          name: 'column and done',
+          data: {
+            [fieldKeys.boardColumn]: 'Doing',
+            [fieldKeys.boardColumnDone]: true
+          },
+          expected: {
+            column: 'Doing',
+            columnDone: true
+          }
+        },
+        {
+          name: 'column, done and lane',
+          data: {
+            [fieldKeys.boardColumn]: 'Validating',
+            [fieldKeys.boardColumnDone]: false,
+            [fieldKeys.boardLane]: 'Urgent'
+          },
+          expected: {
+            column: 'Validating',
+            columnDone: false,
+            lane: 'Urgent'
+          }
+        }
+      ]
     }
   })
 })
