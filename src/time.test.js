a3b085652b69ae8a7e3344a3bc593462f478e1f8
Sylvain Chamberland <gentx2@gmail.com>
2021-03-30 12:25:26 -0400

    removed duplicated tests

--- src/time.test.js
+++ src/time.test.js
@@ -1,28 +1,6 @@
 const getPreviousWorkday = require('./time')
 
 describe('getPreviousWorkDay', function () {
-  const dateSamples = [{
-    name: 'from Tuesday to Friday',
-    data: new Date('2021-03-09T09:09:09Z'),
-    expected: new Date('2021-03-08T09:09:09Z')
-  },
-  {
-    name: 'for Monday',
-    data: new Date('2021-03-15T15:15:15Z'),
-    expected: new Date('2021-03-12T16:15:15Z') // daylight savings time during the weekend
-  },
-  {
-    name: 'for Sunday',
-    data: new Date('2021-03-07T07:07:07Z'),
-    expected: new Date('2021-03-05T07:07:07Z')
-  },
-  {
-    name: 'for January 1st',
-    data: new Date('2023-01-01T01:01:01-0500'),
-    // wrong day if timezone set with UTC (Z) because date manipulation is done with local time
-    expected: new Date('2022-12-30T01:01:01-0500')
-  }]
-
   test.each([
     ['from Tuesday to Friday', new Date('2021-03-09T09:09:09Z'), new Date('2021-03-08T09:09:09Z')],
     // daylight savings time during the weekend
@@ -33,11 +11,5 @@ describe('getPreviousWorkDay', function () {
   ])('returns previous workday %s', (name, data, expected) => {
     const real = getPreviousWorkday(data)
     expect(real).toEqual(expected)
-  })
-  dateSamples.forEach(function (date) {
-    test(`returns previous work day ${date.name}`, function () {
-      const real = getPreviousWorkday(date.data)
-      expect(real).toEqual(date.expected)
-    })
   })
 })
